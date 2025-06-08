48684903cd3a357cb8cc84d260000bfd133999cb
Celso Marques Júnior <celsomarques418@gmail.com>
2025-06-08 14:10:08 -0300

    criando classes e instanciando

---
+++ POO_aula1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Caneta c1 = new Caneta(); // instanciando a classe caneta para criar um objeto
+        
+        c1.modelo = "BIC";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+    }
+}
---
+++ POO_aula1/src/Caneta.java
@@ -0,0 +1,23 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        
+    }
+
+    void rabiscar() {
+
+    }
+    
+    void tampar(){
+
+    }
+
+    void destampar(){
+
+    }
+}
